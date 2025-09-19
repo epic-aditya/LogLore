2eff9d4cf49b447050855bca04324d49acffff95
epic-aditya <mysocialmedia456@gmail.com>
2025-09-19 23:42:21 +0530

    Update API URL to production backend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8000';
+const API_BASE_URL = 'https://loglore-backend.onrender.com';
 
 export const aiTroubleshoot = async (logText, metadata = {}, mode = 'beginner') => {
   try {
